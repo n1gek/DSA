20f8f588322765368cdc1fc48811f2a54b5d7263
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2025-10-08 23:07:58 -0500

    Time: 273 ms (26.86%), Space: 29.2 MB (5.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,42 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0
-        visited = set()
+
+        seen = set()
+
+        def explore(row, col):
+            rowInbound = 0 <= row < rows
+            colInbound = 0 <= col < cols
+
+            if not rowInbound or not colInbound:
+                return
+            
+            if grid[row][col] == "0":
+                return
+            
+            pos = (row, col)
+            if pos in seen:
+                return
+            
+            seen.add(pos)
+            
+            explore(row + 1, col),
+            explore(row, col + 1),
+            explore(row - 1, col),
+            explore(row, col - 1)
+
+        
         rows = len(grid)
         cols = len(grid[0])
-        
+        count = 0
+
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == "1" and (row, col) not in visited:
-                    self.helper(grid, row, col, visited)
+                pos = (row, col)
+                if grid[row][col] == "1" and pos not in seen:
+                    explore(row, col)
                     count += 1
-        return count 
-    
-    
-    def helper(self, grid, row, col, visited):
         
-        row_inbound = 0 <= row < len(grid)
-        col_inbound = 0 <= col < len(grid[0])
-        
-        if not row_inbound or not col_inbound:
-            return 
-        if grid[row][col] == "0":
-            return 
-        
-        pos = (row, col)
-        if pos in visited:
-            return 
-        visited.add(pos)
-        
-        self.helper(grid, row+ 1, col, visited)
-        self.helper(grid, row-1, col, visited)
-        self.helper(grid, row, col-1, visited)
-        self.helper(grid, row, col + 1, visited)
+        return count
+                    
+
         
