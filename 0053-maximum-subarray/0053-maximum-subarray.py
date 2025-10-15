8de764d8b54a9473a24f66810f7db8b1c50ffe9a
Tadiwanashe Nyamapfene <139914488+n1gek@users.noreply.github.com>
2025-10-14 20:35:04 -0500

    Time: 49 ms (78.72%), Space: 32 MB (66.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,24 +1,17 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
 
-        #kadanes algorithm
-        # initialize my max_sum = nums[0]
-        # current_sum = 0
-        # for num in nums:
-        # if current sum < 0: reset it to 0
-        # current_sum += num
-        # get the max between the max and current
-
+        curr_sum = 0
         max_sum = nums[0]
-        current_sum = 0
 
         for num in nums:
-            if current_sum < 0:
-                current_sum = 0
-            
-            current_sum += num
-            max_sum = max(max_sum, current_sum)
+            if curr_sum < 0:
+                curr_sum = 0
+
+            curr_sum += num
+            max_sum = max(max_sum, curr_sum)
         
         return max_sum
             
+
         
